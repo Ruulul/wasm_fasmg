3354655dfad223ef7f90a96265532e9fd666ed3f
Ruulul <vamjunior01@gmail.com>
2023-04-29 19:41:53 -0300

    syntax change; multiple functions assembly success

--- test.asm
+++ test.asm
@@ -1,7 +1,18 @@
 include 'wasm.inc'
 
-func $main param result i32 local
+func (i32) $main()
+start func
     i32.const 42
     return
 end func
+
+func (i32) $double(i32)
+start func
+    local.get 0
+    local.get 0
+    i32.add
+    return
+end func
+
 export func $main as "main"
+export func $double as "2x"
--- test.mjs
+++ test.mjs
@@ -2,4 +2,5 @@ import { readFile } from 'fs/promises'
 const buffer = await readFile('test.wasm')
 const { instance } = await WebAssembly.instantiate(buffer)
 console.log(`the compiled assembly is ${WebAssembly.validate(buffer) ? '' : 'not '}valid!`)
-console.log(instance.exports.main?.call() || "no exported function!")
+for (const [name, func] of Object.entries(instance.exports))
+    console.log(`${name} runs as ${func(2)}`)
